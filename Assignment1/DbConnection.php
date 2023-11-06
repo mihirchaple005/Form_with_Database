cf0f7c736293081ee819ef7d56902a152263f5ed
mihirchaple005 <150040186+mihirchaple005@users.noreply.github.com>
2023-11-06 21:06:35 +0530

    DbConnection.php

--- Assignment1/DbConnection.php
+++ Assignment1/DbConnection.php
@@ -14,4 +14,12 @@ class DbConnection{
         $statement = $pdo->prepare($sql);
         $statement->execute(['Name'=> $Name,'Email'=>$Email,'Gender'=>$Gender,'City'=>$City]);
     }
+
+    public function fetch_data(){
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=assignment1','root','');
+        $statement = $pdo->query('SELECT * FROM assignment');
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        return $statement;
+    }
 }
+
